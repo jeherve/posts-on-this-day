e498e51bfb143f3eb0d884b1de50781c663cf6ea
Jeremy Herve <jeremy@jeremy.hu>
2021-03-19 13:27:29 +0100

    Add build files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-trunk',
+    'version' => 'dev-trunk',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd565b348fdb0e55774c332c9c15393f2944960fa',
+    'name' => 'jeherve/posts-on-this-day',
+  ),
+  'versions' => 
+  array (
+    'automattic/jetpack-autoloader' => 
+    array (
+      'pretty_version' => 'v2.10.0',
+      'version' => '2.10.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6409c42b32ed82aff50869fd0a6a2e5c7a96fbd6',
+    ),
+    'jeherve/posts-on-this-day' => 
+    array (
+      'pretty_version' => 'dev-trunk',
+      'version' => 'dev-trunk',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd565b348fdb0e55774c332c9c15393f2944960fa',
+    ),
+  ),
+);
