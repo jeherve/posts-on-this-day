0c192190b19af3a03e6cde04cce61b58dd7effe4
Jeremy Herve <jeremy@jeremy.hu>
2021-03-29 13:48:22 +0200

    Add test infrastructure

---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * PHPUnit bootstrap file.
+ *
+ * @package jeherve/posts-on-this-day
+ */
+
+/**
+ * Load the composer autoloader.
+ */
+require_once __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * Load WorDBless
+ */
+\WorDBless\Load::load();
